2664bf5a7a891000a4de27f4b5e68474b231f0cf
quanglong29074 <quanglong29074@gmail.com>
2025-07-04 15:27:42 +0700

    tesst 1

--- src/services/blogService.ts
+++ src/services/blogService.ts
@@ -32,9 +32,9 @@ export const updateBlog = async (id: string, title: string, content: string, ima
 
 export const deleteBlog = async (id: string) => {
   const result = await Blog.deleteOne({ _id: id });
-  // if (result.deletedCount === 0) {
-  //   throw new Error('Error deleting blog');
-  // }
+  if (result.deletedCount === 0) {
+    throw new Error('Error deleting blog');
+  }
   return result;
 };
 
