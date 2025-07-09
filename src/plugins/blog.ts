0d8c165eb1ea40ba359d7b36ad2386ecab8e51e5
quanglong29074 <quanglong29074@gmail.com>
2025-07-09 09:47:56 +0700

    test 4

--- src/plugins/blog.ts
+++ src/plugins/blog.ts
@@ -23,74 +23,74 @@ const blogPlugin = new Elysia()
           id: t.String()
         })
       })
-      //  .get("/search", async ({ query }) => {
-      //   const { search } = query;
-      //   return await blogService.searchBlogs(search);
-      // }, {
-      //   detail: {
-      //     tags: ['Blogs']
-      //   },
-      //   query: t.Object({
-      //     search: t.String()
-      //   })
-      // })
-      // .post("/createBlog", async ({ headers, body }) => {
-      //   const token = headers.authorization;
-      //   const loggedUser = isAuthenticated(token);
-      //   const { title, content, image } = body;
-      //   return await blogService.createBlog(title, content, image, loggedUser.id);
-      // }, {
-      //   detail: {
-      //     tags: ['Blogs'],
-      //     security: [
-      //       { JwtAuth: [] }
-      //     ],
-      //   },
-      //   body: t.Object({
-      //     title: t.String(),
-      //     content: t.String(),
-      //     image: t.String()
-      //   })
-      //
-      // })
-      // .put("/updateBlog/:id", async ({ headers, body, params }) => {
-      //   const token = headers.authorization;
-      //   const loggedUser = isAuthenticated(token);
-      //   const { id } = params;
-      //   const { title, content, image } = body;
-      //   return await blogService.updateBlog(id, title, content, image);
-      // }, {
-      //   detail: {
-      //     tags: ['Blogs'],
-      //     security: [
-      //       { JwtAuth: [] }
-      //     ],
-      //   },
-      //   body: t.Object({
-      //     title: t.String(),
-      //     content: t.String(),
-      //     image: t.String()
-      //   }),
-      //   params: t.Object({
-      //     id: t.String()
-      //   })
-      // })
-      // .delete("/deleteBlog/:id", async ({ headers, params }) => {
-      //   const token = headers.authorization;
-      //   const loggedUser = isAuthenticated(token);
-      //   const { id } = params;
-      //   return await blogService.deleteBlog(id);
-      // }, {
-      //   detail: {
-      //     tags: ['Blogs'],
-      //     security: [
-      //       { JwtAuth: [] }
-      //     ],
-      //   },
-      //   params: t.Object({
-      //     id: t.String()
-      //   })
-      // })
+       .get("/search", async ({ query }) => {
+        const { search } = query;
+        return await blogService.searchBlogs(search);
+      }, {
+        detail: {
+          tags: ['Blogs']
+        },
+        query: t.Object({
+          search: t.String()
+        })
+      })
+      .post("/createBlog", async ({ headers, body }) => {
+        const token = headers.authorization;
+        const loggedUser = isAuthenticated(token);
+        const { title, content, image } = body;
+        return await blogService.createBlog(title, content, image, loggedUser.id);
+      }, {
+        detail: {
+          tags: ['Blogs'],
+          security: [
+            { JwtAuth: [] }
+          ],
+        },
+        body: t.Object({
+          title: t.String(),
+          content: t.String(),
+          image: t.String()
+        })
+
+      })
+      .put("/updateBlog/:id", async ({ headers, body, params }) => {
+        const token = headers.authorization;
+        const loggedUser = isAuthenticated(token);
+        const { id } = params;
+        const { title, content, image } = body;
+        return await blogService.updateBlog(id, title, content, image);
+      }, {
+        detail: {
+          tags: ['Blogs'],
+          security: [
+            { JwtAuth: [] }
+          ],
+        },
+        body: t.Object({
+          title: t.String(),
+          content: t.String(),
+          image: t.String()
+        }),
+        params: t.Object({
+          id: t.String()
+        })
+      })
+      .delete("/deleteBlog/:id", async ({ headers, params }) => {
+        const token = headers.authorization;
+        const loggedUser = isAuthenticated(token);
+        const { id } = params;
+        return await blogService.deleteBlog(id);
+      }, {
+        detail: {
+          tags: ['Blogs'],
+          security: [
+            { JwtAuth: [] }
+          ],
+        },
+        params: t.Object({
+          id: t.String()
+        })
+      })
   );
 
 export default blogPlugin;
