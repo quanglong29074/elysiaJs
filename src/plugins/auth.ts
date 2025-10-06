12559eb6e0ebb303ce2e5588486dd4a0597db530
Quang Long <quanglong29074@gmail.com>
2025-10-06 15:43:00 +0700

    test 222

--- src/plugins/auth.ts
+++ src/plugins/auth.ts
@@ -1,40 +1,40 @@
-import { Elysia, t } from 'elysia';
-import * as userService from '../services/userService';
-import { isAuthenticated } from '../middleware/auth';
+// import { Elysia, t } from 'elysia';
+// import * as userService from '../services/userService';
+// import { isAuthenticated } from '../middleware/auth';
 
-const authPlugin = new Elysia()
-  .group("/auth", (group) =>
-    group
-  .get("/users", async ({  }) => {
-    return await userService.getAllUser();
-  }, {
-    detail: {
-      tags: ['auth'],
-    }
-  })
-      .post("/register", async ({ body }) => {
-        return await userService.register(body);
-      }, {
-        detail: {
-          tags: ['auth'],
-        },
-        body: t.Object({
-          username: t.String(),
-          password: t.String()
-        })
-      })
-      .post("/login", async ({ body }) => {
-        return await userService.login(body);
-      }, {
-        detail: {
-          tags: ['auth'],
-        },
-        body: t.Object({
-          username: t.String(),
-          password: t.String()
-        })
-      })
+// const authPlugin = new Elysia()
+//   .group("/auth", (group) =>
+//     group
+//   .get("/users", async ({  }) => {
+//     return await userService.getAllUser();
+//   }, {
+//     detail: {
+//       tags: ['auth'],
+//     }
+//   })
+//       .post("/register", async ({ body }) => {
+//         return await userService.register(body);
+//       }, {
+//         detail: {
+//           tags: ['auth'],
+//         },
+//         body: t.Object({
+//           username: t.String(),
+//           password: t.String()
+//         })
+//       })
+//       .post("/login", async ({ body }) => {
+//         return await userService.login(body);
+//       }, {
+//         detail: {
+//           tags: ['auth'],
+//         },
+//         body: t.Object({
+//           username: t.String(),
+//           password: t.String()
+//         })
+//       })
      
-  );
+//   );
 
-export default authPlugin;
+// export default authPlugin;
